dc8f621c42eeffd6b04b589fdb8082d08540ed69
Luciano Campos <lucianomcampos78@gmail.com>
2021-10-17 20:20:24 -0300

    trabalho-pratico

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import express from 'express'
-const port = 3000
-const app = express()
-app.get('/', (req, res) => {
-  res.json({status: 'Server is running!'})
-})
+import express from 'express';
+const port = 3000;
+import { promises as fs } from 'fs';
+import accountsRouter from './routes/account.routes.js';
+global.fileName = 'accounts.json';
+const app = express();
+app.use(express.json());
+app.use('/account', accountsRouter);
 app.listen(port, () => {
-  console.log(`Server is running at localhost:${port}`)
-})
+  console.log(`Server is running at localhost:${port}`);
+});
